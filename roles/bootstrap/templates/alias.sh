5e13e3619df6d578ef1ad60c88d67ad781070eb6
Víctor Cuadrado Juan <me@viccuad.me>
2017-10-31 19:54:38 +0100

    Add systemwide aliases in bootstrap role

---
+++ roles/bootstrap/templates/alias.sh
@@ -0,0 +1,11 @@
+#
+# Set some generic aliases
+#
+alias o='less'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias cd..='cd ..'
+alias la='ls -la'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
