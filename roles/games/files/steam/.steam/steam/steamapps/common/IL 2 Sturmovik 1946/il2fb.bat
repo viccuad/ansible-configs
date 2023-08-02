054f9793f4e5c1249bcf4f450fd0fc166838065c
Víctor Cuadrado Juan <me@viccuad.me>
2023-08-03 01:01:57 +0200

    games: Add il2 config

---
+++ roles/games/files/steam/.steam/steam/steamapps/common/IL 2 Sturmovik 1946/il2fb.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDWDJFGL+UcMLB5XRzjMMGS5B7kg++D6/e+7kVkYVe8Da5ve27mPMPAA1kbreJsi2EZztIteTChMawCmYRk9oi4T7zDTC86Q4QnjBE2R4ys=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+start "" opentrack/opentrack
+start "" il2fb.exe.bkp %*
